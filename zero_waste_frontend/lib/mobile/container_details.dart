45d4b620141a25de5bf7397576e442639fd53e32
Matej Marinko <matejmarinko123@gmail.com>
2021-05-23 07:06:04 +0200

    container details finished except plots

--- zero_waste_frontend/lib/mobile/container_details.dart
+++ zero_waste_frontend/lib/mobile/container_details.dart
@@ -11,6 +11,8 @@ final bold = TextStyle(
     fontWeight: FontWeight.bold, color: Pallette.primary, fontSize: 17);
 final normal = TextStyle(color: Pallette.primary, fontSize: 17);
 
+const tabs = ['mass', 'impact', 'purchases'];
+
 class ContainerDetails extends StatefulWidget {
   final RContainer container;
 
@@ -22,7 +24,7 @@ class ContainerDetails extends StatefulWidget {
 
 class _ContainerDetailsState extends State<ContainerDetails> {
   charts.Series dataMass, dataWaste, dataCo2;
-  int tab = 0;
+  int currentTab = 0;
 
   @override
   void initState() {
@@ -50,6 +52,65 @@ class _ContainerDetailsState extends State<ContainerDetails> {
       );
     }
 
+    List<Widget> tabWidgets = [];
+    for (int i = 0; i < 3; i++) {
+      tabWidgets.add(Expanded(
+        child: Material(
+          color: i == currentTab ? Pallette.primary : null,
+          child: InkWell(
+            onTap: () => setState(() {
+              currentTab = i;
+            }),
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 4.0),
+              child: Text(
+                tabs[i].toUpperCase(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: i == currentTab ? Colors.white : Pallette.primary,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ));
+    }
+
+    List<Widget> children;
+
+    if (currentTab == 0) {
+      children = [
+        Container(
+          height: 300,
+          child: charts.TimeSeriesChart(
+            [dataMass],
+            animate: true,
+          ),
+        ),
+      ];
+    } else if (currentTab == 1) {
+      children = [
+        Container(
+          height: 300,
+          child: charts.TimeSeriesChart(
+            [dataCo2],
+            animate: true,
+          ),
+        ),
+        Container(
+          height: 300,
+          child: charts.TimeSeriesChart(
+            [dataWaste],
+            animate: true,
+          ),
+        ),
+      ];
+    } else {
+      children = [Container(height: 100.0,)];
+    }
+
     return Scaffold(
         body: DelayedBuilder(
       fadeDuration: Duration(milliseconds: 200),
@@ -70,39 +131,51 @@ class _ContainerDetailsState extends State<ContainerDetails> {
             ],
           ),
           child: SingleChildScrollView(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+            padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 16.0),
             child: Column(
               children: [
-                RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(children: [
-                      TextSpan(text: widget.container.name, style: bold),
-                      TextSpan(
-                          text: ' started it\'s journey at ', style: normal),
-                      TextSpan(text: c.originSeller, style: bold),
-                      TextSpan(text: ' in ', style: normal),
-                      TextSpan(text: c.originLocation, style: bold),
-                      TextSpan(text: ' on ', style: normal),
-                      TextSpan(
-                          text: formatDate(socketApi.util
-                              .toDateTime(widget.container.dateCreated)),
-                          style: bold),
-                      TextSpan(text: '. It transported ', style: normal),
-                      TextSpan(text: '${c.foodG}g', style: bold),
-                      TextSpan(text: ' of food and saved ', style: normal),
-                      TextSpan(text: '${c.co2SavedG}g', style: bold),
-                      TextSpan(
-                          text: ' of waste. There is roughly ', style: normal),
-                      TextSpan(text: '${c.co2SavedG}g', style: bold),
-                      TextSpan(
-                          text: ' less CO2 in the atmosphere thanks to you.',
-                          style: normal),
-                    ])),
-                Container(
-                  height: 360,
-                  child: charts.TimeSeriesChart(
-                    [dataMass],
-                    animate: true,
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 24.0, vertical: 8.0),
+                  child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(children: [
+                        TextSpan(text: widget.container.name, style: bold),
+                        TextSpan(
+                            text: ' started it\'s journey at ', style: normal),
+                        TextSpan(text: c.originSeller, style: bold),
+                        TextSpan(text: ' in ', style: normal),
+                        TextSpan(text: c.originLocation, style: bold),
+                        TextSpan(text: ' on ', style: normal),
+                        TextSpan(
+                            text: formatDate(socketApi.util
+                                .toDateTime(widget.container.dateCreated)),
+                            style: bold),
+                        TextSpan(text: '. It transported ', style: normal),
+                        TextSpan(text: '${c.foodG}g', style: bold),
+                        TextSpan(text: ' of food and saved ', style: normal),
+                        TextSpan(text: '${c.co2SavedG}g', style: bold),
+                        TextSpan(
+                            text: ' of waste. There is roughly ',
+                            style: normal),
+                        TextSpan(text: '${c.co2SavedG}g', style: bold),
+                        TextSpan(
+                            text: ' less CO2 in the atmosphere thanks to you.',
+                            style: normal),
+                      ])),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Container(
+                    color: Colors.grey.shade300,
+                    child: Row(
+                      children: tabWidgets,
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Column(
+                    children: children,
                   ),
                 )
               ],
